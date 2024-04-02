e625302f9ee89b29b16fdd9ddf77b185679b1636
Franck NOUTSA FOTSO <noutsafotso@et.esiea.fr>
2024-04-02 15:41:20 +0200

    Exercice 2, class Orchestra updated and finally finished.

--- src/main/java/fr/esiea/tools/solid/orchestra_ex2/Orchestra.java
+++ src/main/java/fr/esiea/tools/solid/orchestra_ex2/Orchestra.java
@@ -4,41 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Orchestra {
-    private List<Wind> winds = new ArrayList<>();
-    private List<Percussion> percussions = new ArrayList<>();
     private List<Instrument> instruments = new ArrayList<>();
 
-    void tuneWind(Wind wind) {
-        wind.play();
-    }
-
-    void tunePercussion(Percussion percussion) {
-        percussion.play();
-    }
-
-    void tuneInstrument(Instrument instrument) {
+    void tune(Instrument instrument) {
         instrument.play();
     }
 
     void tuneAll() {
-        for (Wind wind : winds) {
-            tuneWind(wind);
-        }
-        for (Percussion percussion : percussions) {
-            tunePercussion(percussion);
-        }
-
         for (Instrument instrument : instruments) {
-            tuneInstrument(instrument);
+            tune(instrument);
         }
-    }
-
-    void addWind(Wind wind) {
-        winds.add(wind);
-    }
-
-    void addPercussion(Percussion percussion) {
-        percussions.add(percussion);
     }
 
     void addInstrument(Instrument instrument) {
